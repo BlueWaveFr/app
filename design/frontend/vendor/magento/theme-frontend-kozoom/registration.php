e7f0c9e8af86643f74ced9ce91fb70657fc1cf8f
BlueWaveFr <stephane@bluewave.fr>
2017-01-12 14:15:43 +0100

    New Theme
    Declare Kozoom Theme

---
+++ design/frontend/vendor/magento/theme-frontend-kozoom/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'vendor/magento/theme-frontend-kozoom',
+    __DIR__
+);
